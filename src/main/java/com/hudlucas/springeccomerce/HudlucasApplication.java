7113b6be0a74df510b78f0ff2b30fecdbaff8477
oTalDoHud <hudson.lucas.vieira@gmail.com>
2022-02-25 22:32:37 -0300

    Projeto Criado

---
+++ src/main/java/com/hudlucas/springeccomerce/HudlucasApplication.java
@@ -0,0 +1,13 @@
+package com.hudlucas.springeccomerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HudlucasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HudlucasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hudlucas/springeccomerce/HudlucasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hudlucas.springeccomerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HudlucasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
