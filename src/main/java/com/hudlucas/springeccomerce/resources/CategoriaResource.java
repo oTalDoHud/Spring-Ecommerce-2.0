5e22067eb7410f6613c2c317fa163a59fb326f94
oTalDoHud <hudson.lucas.vieira@gmail.com>
2022-02-25 22:53:14 -0300

    Categoria rest

--- src/main/java/com/hudlucas/springeccomerce/resources/CategoriaResource.java
+++ src/main/java/com/hudlucas/springeccomerce/resources/CategoriaResource.java
@@ -1,15 +1,27 @@
 package com.hudlucas.springeccomerce.resources;
 
+import com.hudlucas.springeccomerce.domain.Categoria;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 
     @GetMapping
-    public String listar(){
-        return "REST tá funcionando";
+    public List<Categoria> listar(){
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2, "Escritório");
+
+        List<Categoria> list = new ArrayList<>();
+
+        list.add(cat1);
+        list.add(cat2);
+
+        return list;
     }
 }
