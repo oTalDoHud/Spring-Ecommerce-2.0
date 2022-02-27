71e31a6f2b4519284074c9580a70c7b8ffcf9305
oTalDoHud <hudson.lucas.vieira@gmail.com>
2022-02-27 12:41:27 -0300

    Config and instantiation

--- src/main/java/com/hudlucas/springeccomerce/resources/CategoriaResource.java
+++ src/main/java/com/hudlucas/springeccomerce/resources/CategoriaResource.java
@@ -9,28 +9,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @RestController
 @RequestMapping("/categorias")
 public class CategoriaResource {
 
     @Autowired
     CategoriaService service;
-
-    @GetMapping
-    public List<Categoria> listar(){
-        Categoria cat1 = new Categoria(1, "Informática");
-        Categoria cat2 = new Categoria(2, "Escritório");
-
-        List<Categoria> list = new ArrayList<>();
-
-        list.add(cat1);
-        list.add(cat2);
-
-        return list;
-    }
 
     @GetMapping("/{id}")
     public ResponseEntity<Categoria> findById(@PathVariable Integer id){
